4803b4654f11cbb278be286f58260abea453ca55
baiyang <619148077@qq.com>
2018-05-16 11:40:21 +0800

    消除部分冗余代码

--- src/views/errorPages/Page404.vue
+++ src/views/errorPages/Page404.vue
@@ -1,139 +1,127 @@
 <template>
-<div translate="no"  class="body">
-    
+  <div translate="no" class="body">
 
-
-
-
-
-
-<div class="content">
-  <canvas class="snow" id="snow" width="1349" height="400"></canvas>
-  <div class="main-text">
-    <h1>天啊。<br>那页失踪了。</h1>
-    <div class="main-text-a"><a href="#"> 返回首页</a></div>  
-  </div>
-  <div class="ground">
-    <div class="mound"> 
-      <div class="mound_text">404 </div>
-      <div class="mound_spade"></div> 
+    <div class="content">
+      <canvas class="snow" id="snow" width="1349" height="400"></canvas>
+      <div class="main-text">
+        <h1>天啊。<br>那页失踪了。</h1>
+        <div class="main-text-a">
+          <a href="#"> 返回首页</a>
+        </div>
+      </div>
+      <div class="ground">
+        <div class="mound">
+          <div class="mound_text">404 </div>
+          <div class="mound_spade"></div>
+        </div>
+      </div>
     </div>
+
   </div>
-</div>
-
-  
-
-</div>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 
- 
-   export default {
-
-        data () {
-            return {
-           
-            }
-        },
-        mounted(){
-            (function() {
-    function ready(fn) {
-        if (document.readyState != 'loading'){
-            fn();
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    (function() {
+      function ready(fn) {
+        if (document.readyState != "loading") {
+          fn();
         } else {
-            document.addEventListener('DOMContentLoaded', fn);
+          document.addEventListener("DOMContentLoaded", fn);
         }
-    }
-    
-    function makeSnow(el) {
-        var ctx = el.getContext('2d');
+      }
+
+      function makeSnow(el) {
+        var ctx = el.getContext("2d");
         var width = 0;
         var height = 0;
         var particles = [];
-        
+
         var Particle = function() {
-            this.x = this.y = this.dx = this.dy = 0;
-            this.reset();
-        }
-        
+          this.x = this.y = this.dx = this.dy = 0;
+          this.reset();
+        };
+
         Particle.prototype.reset = function() {
-            this.y = Math.random() * height;
-            this.x = Math.random() * width;
-            this.dx = (Math.random() * 1) - 0.5;
-            this.dy = (Math.random() * 0.5) + 0.5;
-        }
-        
+          this.y = Math.random() * height;
+          this.x = Math.random() * width;
+          this.dx = Math.random() * 1 - 0.5;
+          this.dy = Math.random() * 0.5 + 0.5;
+        };
+
         function createParticles(count) {
-            if (count != particles.length) {
-                particles = [];
-                for (var i = 0; i < count; i++) {
-                    particles.push(new Particle());
-                }
+          if (count != particles.length) {
+            particles = [];
+            for (var i = 0; i < count; i++) {
+              particles.push(new Particle());
             }
+          }
         }
-                
+
         function onResize() {
-            width = window.innerWidth;
-            height = window.innerHeight;
-            el.width = width;
-            el.height = height;
-            
-            createParticles((width * height) / 10000);
+          width = window.innerWidth;
+          height = window.innerHeight;
+          el.width = width;
+          el.height = height;
+
+          createParticles(width * height / 10000);
         }
-        
+
         function updateParticles() {
-            ctx.clearRect(0, 0, width, height);
-            ctx.fillStyle = '#f6f9fa';
-            
-            particles.forEach(function(particle) {
-                particle.y += particle.dy;
-                particle.x += particle.dx;
-                
-                if (particle.y > height) {
-                    particle.y = 0;
-                }
-                
-                if (particle.x > width) {
-                    particle.reset();
-                    particle.y = 0;
-                }
-                
-                ctx.beginPath();
-                ctx.arc(particle.x, particle.y, 5, 0, Math.PI * 2, false);
-                ctx.fill();
-            });
-            
-            window.requestAnimationFrame(updateParticles);
+          ctx.clearRect(0, 0, width, height);
+          ctx.fillStyle = "#f6f9fa";
+
+          particles.forEach(function(particle) {
+            particle.y += particle.dy;
+            particle.x += particle.dx;
+
+            if (particle.y > height) {
+              particle.y = 0;
+            }
+
+            if (particle.x > width) {
+              particle.reset();
+              particle.y = 0;
+            }
+
+            ctx.beginPath();
+            ctx.arc(particle.x, particle.y, 5, 0, Math.PI * 2, false);
+            ctx.fill();
+          });
+
+          window.requestAnimationFrame(updateParticles);
         }
-        
+
         onResize();
         updateParticles();
-    }
-    
-    ready(function() {
-        var canvas = document.getElementById('snow');
+      }
+
+      ready(function() {
+        var canvas = document.getElementById("snow");
         makeSnow(canvas);
-    });
-})();
-
-        }
-       
-       
-    }
-
+      });
+    })();
+  }
+};
 </script>
 
 
  <style>
- .body {
+.body {
   height: 700px;
   min-height: 450px;
   font-size: 32px;
   font-weight: 500;
   color: #5d7399;
-  margin:0;padding:0;border:0;
+  margin: 0;
+  padding: 0;
+  border: 0;
 }
 
 .content {
@@ -161,23 +149,24 @@ import Vue from 'vue'
   font-size: 5vh;
 }
 .main-text h1 {
-  font-size:45px;
-  line-height:48px;
-  margin:0;
-  padding:0;}
-.main-text-a{
-  height:32px;
-  margin-left:auto;
-  margin-right:auto;
-  text-align:center;
+  font-size: 45px;
+  line-height: 48px;
+  margin: 0;
+  padding: 0;
+}
+.main-text-a {
+  height: 32px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
 }
 .main-text-a a {
-   font-size:16px;
-   text-decoration:none;
-   color:#0066CC;
- }
-.main-text-a a:hover{
-   color:#000;
+  font-size: 16px;
+  text-decoration: none;
+  color: #0066cc;
+}
+.main-text-a a:hover {
+  color: #000;
 }
 .home-link {
   font-size: 0.6em;
@@ -200,8 +189,9 @@ import Vue from 'vue'
   background: #f6f9fa;
   box-shadow: 0 0 10px 10px #f6f9fa;
 }
-.ground:before, .ground:after {
-  content: '';
+.ground:before,
+.ground:after {
+  content: "";
   display: block;
   width: 250px;
   height: 250px;
@@ -210,17 +200,31 @@ import Vue from 'vue'
   z-index: -1;
   background: transparent;
   -webkit-transform: scaleX(0.2) rotate(45deg);
-          transform: scaleX(0.2) rotate(45deg);
+  transform: scaleX(0.2) rotate(45deg);
 }
 .ground:after {
   left: 50%;
   margin-left: -166.66667px;
-  box-shadow: -340px 260px 15px #8193b2, -620px 580px 15px #8193b2, -900px 900px 15px #b0bccf, -1155px 1245px 15px #b4bed1, -1515px 1485px 15px #8193b2, -1755px 1845px 15px #8a9bb8, -2050px 2150px 15px #91a1bc, -2425px 2375px 15px #bac4d5, -2695px 2705px 15px #a1aec6, -3020px 2980px 15px #8193b2, -3315px 3285px 15px #94a3be, -3555px 3645px 15px #9aa9c2, -3910px 3890px 15px #b0bccf, -4180px 4220px 15px #bac4d5, -4535px 4465px 15px #a7b4c9, -4840px 4760px 15px #94a3be;
+  box-shadow: -340px 260px 15px #8193b2, -620px 580px 15px #8193b2,
+    -900px 900px 15px #b0bccf, -1155px 1245px 15px #b4bed1,
+    -1515px 1485px 15px #8193b2, -1755px 1845px 15px #8a9bb8,
+    -2050px 2150px 15px #91a1bc, -2425px 2375px 15px #bac4d5,
+    -2695px 2705px 15px #a1aec6, -3020px 2980px 15px #8193b2,
+    -3315px 3285px 15px #94a3be, -3555px 3645px 15px #9aa9c2,
+    -3910px 3890px 15px #b0bccf, -4180px 4220px 15px #bac4d5,
+    -4535px 4465px 15px #a7b4c9, -4840px 4760px 15px #94a3be;
 }
 .ground:before {
   right: 50%;
   margin-right: -166.66667px;
-  box-shadow: 325px -275px 15px #b4bed1, 620px -580px 15px #adb9cd, 925px -875px 15px #a1aec6, 1220px -1180px 15px #b7c1d3, 1545px -1455px 15px #7e90b0, 1795px -1805px 15px #b0bccf, 2080px -2120px 15px #b7c1d3, 2395px -2405px 15px #8e9eba, 2730px -2670px 15px #b7c1d3, 2995px -3005px 15px #9dabc4, 3285px -3315px 15px #a1aec6, 3620px -3580px 15px #8193b2, 3880px -3920px 15px #aab6cb, 4225px -4175px 15px #9dabc4, 4510px -4490px 15px #8e9eba, 4785px -4815px 15px #a7b4c9;
+  box-shadow: 325px -275px 15px #b4bed1, 620px -580px 15px #adb9cd,
+    925px -875px 15px #a1aec6, 1220px -1180px 15px #b7c1d3,
+    1545px -1455px 15px #7e90b0, 1795px -1805px 15px #b0bccf,
+    2080px -2120px 15px #b7c1d3, 2395px -2405px 15px #8e9eba,
+    2730px -2670px 15px #b7c1d3, 2995px -3005px 15px #9dabc4,
+    3285px -3315px 15px #a1aec6, 3620px -3580px 15px #8193b2,
+    3880px -3920px 15px #aab6cb, 4225px -4175px 15px #9dabc4,
+    4510px -4490px 15px #8e9eba, 4785px -4815px 15px #a7b4c9;
 }
 
 .mound {
@@ -232,7 +236,7 @@ import Vue from 'vue'
   pointer-events: none;
 }
 .mound:before {
-  content: '';
+  content: "";
   display: block;
   width: 600px;
   height: 200px;
@@ -247,7 +251,7 @@ import Vue from 'vue'
   background-image: linear-gradient(to bottom, #dee8f1, #f6f9fa 60px);
 }
 .mound:after {
-  content: '';
+  content: "";
   display: block;
   width: 28px;
   height: 6px;
@@ -259,13 +263,14 @@ import Vue from 'vue'
   background: #dd4040;
   border-radius: 100%;
   -webkit-transform: rotate(-15deg);
-          transform: rotate(-15deg);
-  box-shadow: -56px 12px 0 1px #dd4040, -126px 6px 0 2px #dd4040, -196px 24px 0 3px #dd4040;
+  transform: rotate(-15deg);
+  box-shadow: -56px 12px 0 1px #dd4040, -126px 6px 0 2px #dd4040,
+    -196px 24px 0 3px #dd4040;
 }
 
 .mound_text {
   -webkit-transform: rotate(6deg);
-          transform: rotate(6deg);
+  transform: rotate(6deg);
 }
 
 .mound_spade {
@@ -278,11 +283,12 @@ import Vue from 'vue'
   margin-right: -250px;
   z-index: 0;
   -webkit-transform: rotate(35deg);
-          transform: rotate(35deg);
+  transform: rotate(35deg);
   background: #dd4040;
 }
-.mound_spade:before, .mound_spade:after {
-  content: '';
+.mound_spade:before,
+.mound_spade:after {
+  content: "";
   display: block;
   position: absolute;
 }
@@ -303,4 +309,4 @@ import Vue from 'vue'
   border: 10px solid #dd4040;
   border-radius: 4px 4px 20px 20px;
 }
- </style>
+</style>
